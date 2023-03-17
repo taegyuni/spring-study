4d79b67fcb8aa8fd115d80aba2808fbbfaf19862
taegyuni <taegyun7553@naver.com>
2023-03-18 02:00:30 +0900

    controller 생성

--- hello-spring/src/main/java/hello/hellospring/HelloSpringApplication.java
+++ hello-spring/src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -2,23 +2,11 @@ package hello.hellospring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class HelloSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloSpringApplication.class, args);
-	}
-
-	@Controller
-	public class HelloController {
-		@GetMapping("hello")
-		public String hello(Model model) {
-			model.addAttribute("data", "hello!!");
-			return "hello";
-		}
 	}
 }
