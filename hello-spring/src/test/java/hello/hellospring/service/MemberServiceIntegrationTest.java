e409cbeef92bc2533941f64af7661626669d093f
taegyuni <taegyun7553@naver.com>
2023-06-11 15:41:58 +0900

    스프링 데이터 JPA 활용

--- hello-spring/src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
+++ hello-spring/src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -21,7 +21,7 @@ class MemberServiceIntegrationTest {
     void 회원가입() {
         //given
         Member member = new Member();
-        member.setName("spring");
+        member.setName("spring43");
 
         //when
         Long saveId = memberService.join(member);
